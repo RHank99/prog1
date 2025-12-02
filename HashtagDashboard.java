157958994780e1493902681d54e848fcba58a059
AaronJF164 <aaron-jerome.folmeg@informatik.hs-fulda.de>
2025-12-02 15:12:50 +0100

    HA06

---
+++ HashtagDashboard.java
@@ -0,0 +1,11 @@
+public class HashtagDashboard {
+    public static void main(String[] args) {
+        
+    }
+}
+
+//Konsolenargumente werden in im Array args gespeichert
+//Hashtags auslesen
+//Hashtags im Array speichern (max. 100 Stück)
+//Hashtags im Array lesen wie oft derjenige vorkommt im Array
+//dafür vermutlich lowercase umwandeln
